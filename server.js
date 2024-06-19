02705e1b5dc0f34d7db1c513774603ecf0c41d9e
Abhaya <61579704+abhaya-kumar-sahoo@users.noreply.github.com>
2024-06-19 15:25:25 +0530

    cors added

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const userRoutes = require("./routes/userRoutes");
 const urlRoutes = require("./routes/urlRoutes");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3008;
 const DB_URI = "mongodb+srv://dbUser:abhayaks@cluster0.a0wvhwr.mongodb.net/";
 // WYHbdCbygXg74QGl
 app.use(bodyParser.json());
+app.use(cors());
 
 mongoose
   .connect(DB_URI)
