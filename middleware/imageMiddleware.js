f6c85669713a2991cf8a4a9798a73b015c6ca19d
Shashikalwar <163123388+Shashikalwar@users.noreply.github.com>
2024-07-20 13:22:15 +0530

    changes

--- middleware/imageMiddleware.js
+++ middleware/imageMiddleware.js
@@ -29,6 +29,25 @@ const s3Storage = multerS3({
 
 const upload_images = multer({
     storage: s3Storage,
+    limits: {
+        fileSize: 20 * 1024 * 1024 // 10 MB size limit
+    // },
+    // fileFilter: (req, file, cb) => {
+    //     // Allow all files except audio files (.wav and .mp3 only)
+    //     if (file.mimetype.startsWith("audio/")) {
+    //         const filetypes = /wav|mp3/;
+    //         const extname = filetypes.test(file.originalname.toLowerCase());
+
+    //         if (extname) {
+    //             return cb(null, true);
+    //         } else {
+    //             return cb(new Error("Only .wav and .mp3 audio files are allowed!"));
+    //         }
+    //     }
+
+    //     // Allow all non-audio files
+    //     cb(null, true);
+    }
 });
 
 module.exports = upload_images;
--- models/InBoundCallStatusModel.js
+++ models/InBoundCallStatusModel.js
@@ -7,7 +7,7 @@ const CallsStatusSchema = new mongoose.Schema(
         overAllAnswered: { type: Number, required: true },
         overAllAbandoned: { type: Number, required: true },
         answeredPercentage: { type: Number, required: true },
-        ACHT: { type: Number, required: true },
+        ACHT: { type: String, required: true },
         graph: {type:String, required: false },
         image: {type:String, required: false },
         userId: {
